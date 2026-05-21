90d6315283f5c0c03562aaddf12b4d6bff92d4c8
juanp <juanpabloj32@gmail.com>
2026-05-21 13:21:00 -0600

    Actualizacion de la activity_login.xml

--- app/src/main/java/com/miempresa/proyectofinal/LoginActivity.java
+++ app/src/main/java/com/miempresa/proyectofinal/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.miempresa.proyectofinal;
 
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,9 +25,28 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         btnEntrar = findViewById(R.id.btnEntrar);
 
+        // ANIMACIÓN
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        btnEntrar.startAnimation(animation);
+
         btnEntrar.setOnClickListener(v -> {
 
-            startActivity(new Intent(LoginActivity.this, ProductosActivity.class));
+            String user = usuario.getText().toString();
+            String pass = password.getText().toString();
+
+            if(user.equals("admin") && pass.equals("1234")) {
+
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+
+            } else {
+
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            }
 
         });
     }
